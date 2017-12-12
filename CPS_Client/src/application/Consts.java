4b0d811a079fa391416f29fe7bf968bfc4def905
yuval <yuvalzernik.yz@@gmail.com>
2017-12-12 17:44:43 +0200

    test yuval

--- CPS_Client/src/application/Consts.java
+++ CPS_Client/src/application/Consts.java
@@ -2,12 +2,20 @@ package application;
 
 public class Consts
 {
+    public static final String InputsAreIncorrect = "Please fill in all fields as required";
+    
+    public static final String ThankYouForRegistering = "Thank you for registering the system. \nWe how you enjoy!";
+    
+    public static final String ParkinglotNamePathFromController = "..\\ParkinglotName.txt";
+    
+    public static final String LeaveTheParkinglotMessage = "You can take the car. \n Have a nice day!";
+    
     public static final String LeaveTheCarMessage = "Please turn off the vehicle and leave it here. \n Have a nice day!";
     
     public static final String Approved = "Request approved";
     
     public static final String FullMembership = "Full Membership";
-
+    
     public static final String PartialMembership = "Partial Membership";
     
     public static final String FillRequest = "Please fill out all fields";
@@ -21,23 +29,31 @@ public class Consts
     public static final String Kiosk = "Kiosk Main";
     
     public static final String KioskEntryFxmlPath = "Fxmls/KioskEntry.fxml";
-
+    
     public static final String KioskEntry = "Kiosk Entry";
     
     public static final String MonitorAndControllFxmlPath = "Fxmls/MonitorAndControll.fxml";
-
+    
     public static final String MonitorAndControll = "Monitor And Controll";
     
     public static final String KioskExitFxmlPath = "Fxmls/KioskExit.fxml";
-
+    
     public static final String KioskExit = "Kiosk Exit";
     
-    public static final String FullMembershipRegisterFxmlPath ="Fxmls/FullMembershipRegister.fxml";
+    public static final String FullMembershipRegisterFxmlPath = "Fxmls/FullMembershipRegister.fxml";
     
-    public static final String FullMembershipRegister ="Full Membership Register";
+    public static final String FullMembershipRegister = "Full Membership Register";
     
-    public static final String PartialMembershipRegisterFxmlPath ="Fxmls/PartialMembershipRegister.fxml";
+    public static final String PartialMembershipRegisterFxmlPath = "Fxmls/PartialMembershipRegister.fxml";
     
-    public static final String PartialMembershipRegister ="Partial Membership Register";
+    public static final String PartialMembershipRegister = "Partial Membership Register";
+    
+    public static final String GuestEntryFxmlPath = "Fxmls/Guest_Entry.fxml";
+    
+    public static final String GuestEntry = "Guest Entry";
+    
+    public static final String PaymentFxmlPath = "Fxmls/Payment.fxml";
+    
+    public static final String Payment = "Payment";
     
 }
