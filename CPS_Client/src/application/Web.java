cb1d4d20bd63a124227eab8943acd13ea2331390
yuval <yuvalzernik.yz@@gmail.com>
2017-12-12 17:26:36 +0200

    update yuval 12/12

---
+++ CPS_Client/src/application/Web.java
@@ -0,0 +1,41 @@
+package application;
+
+import java.net.URL;
+import java.util.LinkedList;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.util.Pair;
+
+public class Web extends Application
+{
+    @Override
+    public void start(Stage primaryStage)
+    {
+	try
+	{
+	    LinkedList<Pair<String, URL>> list = new LinkedList<>();
+	    
+	    Class<?> myClass = getClass();
+	     
+	    list.add(new Pair<String, URL>(ConstsWeb.Web, myClass.getResource(ConstsWeb.WebFxmlPath)));
+	    list.add(new Pair<String, URL>(Consts.MonitorAndControll, myClass.getResource(Consts.MonitorAndControllFxmlPath)));
+	    list.add(new Pair<String, URL>(Consts.FullMembershipRegister, myClass.getResource(Consts.FullMembershipRegisterFxmlPath)));
+	    list.add(new Pair<String, URL>(Consts.PartialMembershipRegister, myClass.getResource(Consts.PartialMembershipRegisterFxmlPath)));
+	    list.add(new Pair<String, URL>(ConstsWeb.Complaints, myClass.getResource(ConstsWeb.ComplaintsFxmlPath)));
+	    list.add(new Pair<String, URL>(ConstsWeb.OrderInAdvance, myClass.getResource(ConstsWeb.OrderInAdvanceFxmlPath)));
+
+	    ControllersManager controllersManager = new ControllersManager(list, primaryStage);
+	    
+	    controllersManager.SetScene(ConstsWeb.Web, null);
+	}
+	catch (Exception e)
+	{
+	    e.printStackTrace();
+	}
+    }
+    
+    public static void main(String[] args)
+    {
+	launch(args);
+    }
+}
