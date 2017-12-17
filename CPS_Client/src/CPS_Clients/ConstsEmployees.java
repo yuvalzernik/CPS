3bec924cdeb9091902a19a7c2c9efb5a27a67108
barhassin <34250459+barhassin@users.noreply.github.com>
2017-12-17 19:52:37 +0200

    revital new branch (#5)

---
+++ CPS_Client/src/CPS_Clients/ConstsEmployees.java
@@ -0,0 +1,8 @@
+package CPS_Clients;
+
+public class ConstsEmployees {
+	 public static final String Employees ="Employees Main";
+	 public static final String EmployeesLoginFxmlPath ="Fxmls/EmployeesLogin.fxml";
+	 public static final String EmployeesLogin="Employees Login";
+	 
+}
---
+++ CPS_Client/src/CPS_Clients/Controllers/ParkingLotWorkerEnteryController.java
@@ -0,0 +1,34 @@
+package CPS_Clients.Controllers;
+
+import java.awt.event.ActionEvent;
+
+import javafx.fxml.FXML;
+
+public class ParkingLotWorkerEnteryController {
+
+    @FXML
+    void OnInitializeParkingLot(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnRegisterDisabeledParkingLot(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnReserveParkingLot(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnRegisterDisabeledParkingSpot(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onBack(ActionEvent event) {
+
+    }
+
+}
