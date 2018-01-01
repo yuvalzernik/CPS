5e8688285c6f38b27881ef02433c2c68fb53e799
barhassin <34250459+barhassin@users.noreply.github.com>
2018-01-01 19:55:30 +0200

    commit message (#17)

---
+++ CPS_Client/src/entities/DisabledParkingSpot.java
@@ -0,0 +1,65 @@
+package entities;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import entities.enums.ParkingSpotStatus;
+
+public class DisabledParkingSpot implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+	
+	private String parkingLotName;
+	
+	private ParkingSpot parkingSpot;
+	
+	private LocalDateTime startDateTime;
+	
+	private LocalDateTime endDateTime;
+	
+	private ParkingSpotStatus status;
+	
+	public DisabledParkingSpot(String parkingLotName, ParkingSpot parkingSpot, LocalDateTime startDateTime, LocalDateTime endDateTime, ParkingSpotStatus status)
+	{
+		this.parkingLotName=parkingLotName;
+		
+		this.parkingSpot=parkingSpot;
+		
+		this.startDateTime=startDateTime;
+		
+		this.endDateTime=endDateTime;
+		
+		this.status=status;
+	}
+	
+	public String getParkingLotName()
+	{
+		return parkingLotName;
+	}
+	
+	public ParkingSpot getParkingSpot()
+	{
+		return parkingSpot;
+	}
+	
+	public LocalDateTime getStartDateTime()
+	{
+		return startDateTime;
+	}
+	
+	public LocalDateTime getEndDateTime()
+	{
+		return endDateTime;
+	}
+	
+	public ParkingSpotStatus getStatus()
+	{
+		return status;
+	}
+	
+	@Override
+    public String toString()
+    {
+		return "Parking lot name: " + parkingLotName;
+    }
+}
